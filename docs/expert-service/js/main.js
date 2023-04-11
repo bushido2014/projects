f9f4f05448736c32437fff4c5f49114b959c531f
Viorel Soltan <joomjoom2011@gmail.com>
2023-04-11 12:44:05 +0000

    nav

---
+++ docs/expert-service/js/main.js
@@ -0,0 +1,21 @@
+// получаем кнопку "скролл на вверх"
+var toTopButton = document.querySelector('.to-top');
+
+// добавляем прослушиватель событий для отслеживания прокрутки страницы
+window.addEventListener('scroll', function() {
+  // если прокрутка больше 100 пикселей, показываем кнопку
+  if (window.pageYOffset > 100) {
+    toTopButton.classList.add('show');
+  } else {
+    toTopButton.classList.remove('show');
+  }
+});
+
+// добавляем прослушиватель событий для кнопки "скролл на вверх"
+toTopButton.addEventListener('click', function() {
+  // прокручиваем страницу вверх
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
