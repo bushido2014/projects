7ba37a10d68117b52b274dfa2c9f56f0b7ae1d2d
Viorel Soltan <joomjoom2011@gmail.com>
2023-04-11 11:12:09 +0000

    menu|

--- docs/expert-service/js/main.js
+++
@@ -1,6 +0,0 @@
-const navMobile = document.querySelector(".nav-mobile");
-const navbar = document.querySelector(".nav");
-navMobile.addEventListener("click", () => {
-  navMobile.classList.toggle("mb-active");
-  navbar.classList.toggle("mb-overlay");
-}); 
