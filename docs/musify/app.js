897e39bea76caa91b304702d54ce4362cfc3e3b6
Viorel Soltan <joomjoom2011@gmail.com>
2023-08-29 13:21:17 +0000

    me2

--- docs/musify/app.js
+++ docs/musify/app.js
@@ -2,3 +2,19 @@ const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark")
 })
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
+
+// let resizeTimer;
+// window.addEventListener("resize", () => {
+//   document.body.classList.add("resize-animation-stopper");
+//   clearTimeout(resizeTimer);
+//   resizeTimer = setTimeout(() => {
+//     document.body.classList.remove("resize-animation-stopper");
+//   }, 400);
+// });
