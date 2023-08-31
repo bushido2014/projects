8bd610ebdda2a297a53faa00389937b46bb838e6
Viorel Soltan <joomjoom2011@gmail.com>
2023-08-31 10:22:52 +0000

    media

--- docs/musify/app.js
+++ docs/musify/app.js
@@ -9,3 +9,12 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navbar.classList.toggle("active");
 });
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
