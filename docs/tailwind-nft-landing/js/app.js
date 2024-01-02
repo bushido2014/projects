6a502a3d22827742b8413d00ccc647e81bba48cc
Viorel Soltan <joomjoom2011@gmail.com>
2024-01-02 12:03:38 +0000

    alp

--- docs/tailwind-nft-landing/js/app.js
+++ docs/tailwind-nft-landing/js/app.js
@@ -40,3 +40,20 @@ const testimoinal = new Swiper(".collection", {
     },
   },
 });
+
+function slideout() {
+	return {
+		open: false,
+		usedKeyboard: false,
+		init() {
+			this.$watch('open', value => {
+				value && this.$refs.closeButton.focus()
+				this.toggleOverlay()
+			})
+			this.toggleOverlay()
+		},
+		toggleOverlay() {
+			document.body.classList[this.open ? 'add' : 'remove']('h-screen', 'overflow-hidden')
+		}
+	}
+}
